f86665cf6f4bf201c876b6884e77ea5cfc274674
kou <k.ogawa.biz406@gmail.com>
2025-03-17 19:39:40 +0900

    fix: 要約機能と翻訳機能のガワだけ作って分けてみる

--- src/quick-translate.tsx
+++ src/quick-translate.tsx
@@ -1,12 +1,12 @@
-import { getSelectedText, launchCommand, LaunchType, getPreferenceValues } from "@raycast/api";
+import { getSelectedText, launchCommand, LaunchType } from "@raycast/api";
 
 export default async function Command() {
   const selectedText = await getSelectedText();
-  console.log(`📝 SelectedTextGetCommand: 実行されました（現在時刻：${new Date()}）`);
+  console.log(`📝 SelectedTextGetCommandForTranslate: 実行されました（現在時刻：${new Date()}）`);
   console.log("selectedText", selectedText);
 
   await launchCommand({
-    name: "summarize-text-command",
+    name: "translate-result-view-DO-NOT-USE-DIRECTLY",
     type: LaunchType.UserInitiated,
     context: {
       inputText: selectedText,
